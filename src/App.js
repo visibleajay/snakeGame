04a85cfe13694c32c2ba9255a2abdecff8bc641a
Ajay Kumar <visibleajay@gmail.com>
2019-07-17 22:58:15 +0530

    Remove unused redux from app.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,10 @@ import React from 'react';
 import './App.css';
 import GameArea from './GameArea/GameArea.js';
 
-import { Provider } from 'react-redux';
-import Store from './Store';
-
 function App() {
   return (
     <div className="App">
-      <Provider store={Store}>
-        <GameArea />  
-      </Provider>
+      <GameArea />  
     </div>
   );
 }
