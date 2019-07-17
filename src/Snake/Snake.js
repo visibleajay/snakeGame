7f967a7cfdca63760792095b09978d0b7e9da4b8
Ajay Kumar <visibleajay@gmail.com>
2019-07-17 10:37:29 +0530

    Clean Snake comp.

--- src/Snake/Snake.js
+++ src/Snake/Snake.js
@@ -4,19 +4,7 @@ import Box from './Box/Box.js';
 
 import "./Snake.css";
 
-// // function reducer( state, action ) {
-// //     switch (key) {
-// //         case value:
-            
-// //             break;
-
-// //         default:
-// //             return state;
-// //     }
-// // }
-
-
-const moveSnake = (newPosition, gameBoundary, foodPos, snakePos, onFoodEat) => {
+const moveSnake = (newPosition, gameBoundary, foodPos, snakePos) => {
 
     const isTouchedBoundary = ({x, y}, {left, top, right, bottom}) => 
                                 x < left || x + 20 > right || y + 20 > bottom || y < top;
@@ -24,23 +12,11 @@ const moveSnake = (newPosition, gameBoundary, foodPos, snakePos, onFoodEat) => {
     const isAteItself = ({x: newPosX, y: newPosY}) => snakePos.some( ({x,y}) => x === newPosX && y === newPosY);
 
     if ( isTouchedBoundary(newPosition, gameBoundary) || isAteItself(newPosition) ) {
-        // this.isGameOver     =   true;
-        // clearInterval(this.intervalId);
         return [];
     }
-    
 
-    // if (  ) {
-        // this.isGameOver     =   true;
-        // clearInterval(this.intervalId);
-    //     onGameOver();
-    //     return ;
-    // }
-
-    // if ( newPosition ) {
     let snakePositions  =   [];
     const {x: foodPosX, y: foodPosY} = foodPos;
-    // this.id += 1;
     
     if ( newPosition.x === foodPosX && newPosition.y === foodPosY ) {
         snakePositions = [  newPosition, ...snakePos.slice() ];
@@ -49,54 +25,14 @@ const moveSnake = (newPosition, gameBoundary, foodPos, snakePos, onFoodEat) => {
     }
 
     return snakePositions;
-        // this.setState({
-        //     "snakePos": snakePositions
-        // });
-    // };
 }
-
-// const computeSnakeNextPosition = useCallback( ([ frontSnakePosition, secondSnakePosition, ...rest ]) => {
-//     // const  = this.state.snakePos[0];
-//     // const   = this.state.snakePos[1];
-
-//     const isGoingLeft = ({x: currentPosX, y: currentPosY}, {x: secondPosX, y: secondPosY}) => currentPosX < secondPosX && currentPosY === secondPosY;
-//     const isGoingUp = ({x: currentPosX, y: currentPosY}, {x: secondPosX, y: secondPosY}) => currentPosX === secondPosX && currentPosY < secondPosY;
-//     const isGoingRight = ({x: currentPosX, y: currentPosY}, {x: secondPosX, y: secondPosY}) => currentPosX > secondPosX && currentPosY === secondPosY;
-//     const isGoingDown = ({x: currentPosX, y: currentPosY}, {x: secondPosX, y: secondPosY}) => currentPosX === secondPosX && currentPosY > secondPosY;
-
-//     let newPosition   =   null;
-//     if ( isGoingLeft(frontSnakePosition, secondSnakePosition) ) {
-//         newPosition   =   { "x": frontSnakePosition.x - 20, "y": frontSnakePosition.y, id: this.id };
-//     } 
-
-//     if ( isGoingRight(frontSnakePosition, secondSnakePosition) ) {
-//         newPosition   =   { "x": frontSnakePosition.x + 20, "y": frontSnakePosition.y, id: this.id };
-//     } 
-
-//     if ( isGoingDown(frontSnakePosition, secondSnakePosition) ) {
-//         newPosition   =   { "x": frontSnakePosition.x, "y": frontSnakePosition.y + 20, id: this.id };
-//     } 
-
-//     if ( isGoingUp(frontSnakePosition, secondSnakePosition) ) {
-//         newPosition   =   { "x": frontSnakePosition.x, "y": frontSnakePosition.y - 20, id: this.id };
-//     } 
-
-//     // if ( newPosition ) {
-//         // this.updateSnakeUI(newPosition);
-//     return newPosition;
-//     // }
-
-// }, [statePos]);
-
 
 const snakePosition         = ( keyCode, currentPosition, isGameOver ) => {
     if ( isGameOver ) return null;
 
     const leftKey = 37;
-    // const currentPosition = snakePos[0];
     let newPosition =   null;
 
-    // console.log("current position ", currentPosition);
     if ( keyCode === leftKey ) {
         newPosition = {"x": currentPosition.x - 20 , "y": currentPosition.y};
     }
@@ -117,9 +53,6 @@ const snakePosition         = ( keyCode, currentPosition, isGameOver ) => {
     }
 
     return newPosition;
-    // if ( newPosition ) {
-    //     this.updateSnakeUI(newPosition)
-    // }
 };
 
 
@@ -130,9 +63,7 @@ function Snake({foodPos, gameBoundary, keyCode, onGameOver, onFoodEat}) {
     ];
     const refContainer  =   useRef(null);
     const [snakePos, updateSnakePos] = useState(initialSnakePositions)
-    const [isGameOver, setGameOver]  = useState(false);    
-    // const memoizedSnakePosition = useCallback( snakePosition, [keyCode] )
-    // const memoizedMoveSnake = useCallback( moveSnake, [newPosition]);
+    const [isGameOver, setGameOver]  = useState(false);
 
     useEffect( () => {
         refContainer.current = setInterval( () => {
@@ -151,14 +82,6 @@ function Snake({foodPos, gameBoundary, keyCode, onGameOver, onFoodEat}) {
         }
     }, [foodPos, snakePos, keyCode]);
 
-
-
-
-    // useEffect( () => {
-    //     changeSnakeDirection(keyCode);
-
-    // }, [keyCode, snakePos])
-
     return (
         snakePos.map( ({x: left, y: top, id}, index) => {
             return <Box key={index} left={left} top={top} />
@@ -166,52 +89,5 @@ function Snake({foodPos, gameBoundary, keyCode, onGameOver, onFoodEat}) {
     )
 }
 
-
-// class Snake extends React.PureComponent {
-
-//     constructor(props) {
-//         super(props);
-//         const initialSnakePositions =   [
-//             {"x": 200, "y": 100, "id": 0 },
-//             {"x": 220, "y": 100, "id": 1 }
-//         ];
-//         this.id         =   5;
-//         this.isGameOver =   false;
-//         this.state      =   {"snakePos": initialSnakePositions};
-//     }
-
-//     componentDidMount () {
-//         this.intervalId   =   setInterval(() => this.moveSnakeToNextPosition(), 150);
-//         this.moveSnakeToNextPosition();
-//     }
-    
-//     componentWillMount() {
-//         clearInterval(this.intervalId);
-//     }
-
-//     // moveSnakeToNextPosition() {
-
-//     // }
-
-//     // renderSnake =   (newPosition) => {
-
-//     // }
-
-//     changeSnakeDirection({ keyCode }){
-
-
-//     }
-
-//     componentDidUpdate(prevProps) {
-//         if ( prevProps.keyCode !== this.props.keyCode ) {
-//             this.changeSnakeDirection(this.props);
-//         }
-//     }
-
-//     render() {
-        
-//     }
-
-// }
-
 export default Snake;
+
